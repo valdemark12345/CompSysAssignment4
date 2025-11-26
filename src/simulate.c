db48fb449be62ce42b6c77738e8cdbce2e9cd19f
LasseHelmer <lasse.helmer.larsen@gmail.com>
2025-11-26 13:29:19 +0100

    Added dummy things

--- src/simulate.c
+++ src/simulate.c
@@ -1,2 +1,9 @@
+#include "memory.h"
+#include "read_elf.h"
+#include "simulate.h"
+#include <stdio.h>
 
-
+struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct symbols* symbols){
+    struct Stat dummy;
+    return dummy;
+}
