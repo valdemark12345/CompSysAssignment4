063d333df12c756725ae92587b934fd33c691ad4
Valdemar Kragh <valdemarkragh@Valdemars-MacBook-Pro.local>
2025-11-26 13:20:21 +0100

    Start disassemble / simulate

---
+++ src/disassemble.c
@@ -0,0 +1,8 @@
+#include "disassemble.h"
+#include "memory.h"
+#include <stdio.h>
+#include <stdint.h>
+
+void disassemble(uint32_t addr, uint32_t instruction, char* result, size_t buf_size, struct symbols* symbols){
+    
+}
