f3dbb1f8f2b2cb88115a0dfd648e33ac45f71ae2
Valdemar Kragh <valdemarkragh@Valdemars-MacBook-Pro.local>
2025-11-30 12:25:54 +0100

    Ændret ABI names

--- src/disassemble.c
+++ src/disassemble.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
+//ABI names ordered
 static const char *reg_names[32] = {
-    "zero","ra","sp","gp","tp","t0","t1","t2",
+    "x0","ra","sp","gp","tp","t0","t1","t2",
     "s0","s1","a0","a1","a2","a3","a4","a5",
     "a6","a7","s2","s3","s4","s5","s6","s7",
     "s8","s9","s10","s11","t3","t4","t5","t6"
