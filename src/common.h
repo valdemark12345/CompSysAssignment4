69bfbbfe3ce39edf2040d06db1e78414f33f0c55
LasseHelmer <lasse.helmer.larsen@gmail.com>
2025-12-01 16:10:29 +0100

    Changed implementation of CPU struct slightly

--- src/common.h
+++ src/common.h
@@ -13,44 +13,13 @@ static const char *reg_names[32] = {
 };
 
 struct CPU {
-    uint32_t zero;
-    uint32_t ra;
-    uint32_t sp;
-    uint32_t gp;
-    uint32_t tp;
-    uint32_t t0;
-    uint32_t t1;
-    uint32_t t2;
-    uint32_t s0;
-    uint32_t s1;
-    uint32_t a0;
-    uint32_t a1;
-    uint32_t a2;
-    uint32_t a3;
-    uint32_t a4;
-    uint32_t a5;
-    uint32_t a6;
-    uint32_t a7;
-    uint32_t s2;
-    uint32_t s3;
-    uint32_t s4;
-    uint32_t s5;
-    uint32_t s6;
-    uint32_t s7;
-    uint32_t s8;
-    uint32_t s9;
-    uint32_t s10;
-    uint32_t s11;
-    uint32_t t3;
-    uint32_t t4;
-    uint32_t t5;
-    uint32_t t6;
+    uint32_t registers[32];
     uint32_t pc;
 };
 
 struct CPU* init_cpu(){
     struct CPU* cpu = malloc(sizeof(struct CPU));
-    cpu->zero = 0;
+    cpu->registers[0] = 0;
     return cpu;
 }
 
--- src/simulate.c
+++ src/simulate.c
@@ -4,7 +4,15 @@
 #include "common.h"
 #include <stdio.h>
 
+struct CPU* cpu;
+
+
 struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct symbols* symbols){
+    cpu = init_cpu();
     struct Stat dummy;
     return dummy;
+}
+
+void add(int dest, int reg1, int reg2){
+    cpu->registers[dest] = cpu->registers[reg1] + cpu->registers[reg2];
 }
