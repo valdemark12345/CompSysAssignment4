e9729afba988b5fc1aa7beddea331a988824f661
LasseHelmer <lasse.helmer.larsen@gmail.com>
2025-12-03 11:28:55 +0100

    Added more boring instructions

--- src/simulate.c
+++ src/simulate.c
@@ -13,6 +13,12 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file, struct 
     return dummy;
 }
 
+int load_word_from_memory(struct memory *mem){
+    return (memory_rd_w(mem, cpu.pc));
+}
+
+
+
 // R-types
 
 void add(int dest, int reg1, int reg2){
@@ -23,15 +29,54 @@ void mul(int dest, int reg1, int reg2){
     cpu.registers[dest] = cpu.registers[reg1] * cpu.registers[reg2];
 }
 
+void sub(int dest, int reg1, int reg2){
+    cpu.registers[dest] = cpu.registers[reg1] - cpu.registers[reg2];
+}
 
-void lui(int dest, int upper_immediate){
+void xor(int dest, int reg1, int reg2){
+    cpu.registers[dest] = cpu.registers[reg1] ^ cpu.registers[reg2];
+}
+
+void or(int dest, int reg1, int reg2){
+    cpu.registers[dest] = cpu.registers[reg1] | cpu.registers[reg2];
+}
+
+void and(int dest, int reg1, int reg2){
+    cpu.registers[dest] = cpu.registers[reg1] & cpu.registers[reg2];
+}
+
+
+
+//I-types
+
+int check_immediate(int immediate){
+    if (immediate > 0xfff){
+        printf("Immediate was too large! \n");
+        return 0;
+    }
+    else return 1;
+}
+
+
+// U types
+
+int check_upper_immediate(int upper_immediate){
     if (upper_immediate > 0xfffff){
         printf("upper_immediate was too large! \n");
-        return;
-
+        return 0;
     }
-    else {
+    else return 1;
+}
+
+void lui(int dest, int upper_immediate){
+    if (check_upper_immediate(upper_immediate)){
         cpu.registers[dest] = upper_immediate;
         cpu.registers[dest] << 12;
+    }
+}
+
+void auipc(int dest, int upper_immediate){
+    if (check_upper_immediate(upper_immediate)){
+    cpu.registers[dest] = cpu.pc + (upper_immediate << 12);
     }
 }
